be31bf76674a25e0107087f66316a1b099228665
Rohit Tomar <tomarrohit5034@gmail.com>
2022-11-29 14:05:23 +0000

    doubt

---
+++ src/Components/UserProfile/index.js
@@ -0,0 +1,11 @@
+const UserProfile = props => {
+  const {userDetails} = props
+  const {imageUrl} = userDetails
+
+  return (
+    <div>
+      <img src={imageUrl} />
+    </div>
+  )
+}
+export default UserProfile
